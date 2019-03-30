9cf26df88fd558b8bfbbe8328ec73e8bf272f575
Toshiaki Maki <tmaki@pivotal.io>
2019-03-31 04:43:28 +0900

    Tweak

--- src/main/java/am/ik/pks/client/PksApiClient.java
+++ src/main/java/am/ik/pks/client/PksApiClient.java
@@ -10,18 +10,31 @@ public class PksApiClient {
 
     public Flux<PksCluster> getClusters() {
         return Flux.just(new PksClusterBuilder()
-            .setUuid("d2adf0c1-9401-44eb-b479-8c42abc05c6b")
-            .setName("demo1")
-            .setLastAction("SUCCEEDED")
-            .setKubernetesMasterIps(Arrays.asList("54.64.203.141", "13.113.212.19"))
-            .setParameters(new PksCluster.Parameters() {
+                .setUuid("d2adf0c1-9401-44eb-b479-8c42abc05c6b")
+                .setName("demo1")
+                .setLastAction("SUCCEEDED")
+                .setKubernetesMasterIps(Arrays.asList("54.64.203.141"))
+                .setParameters(new PksCluster.Parameters() {
 
-                {
-                    setKubernetesMasterHost("demo1.pks.example.com");
-                    setKubernetesMasterPort(443);
-                }
-            })
-            .createPksCluster());
+                    {
+                        setKubernetesMasterHost("demo1.pks.example.com");
+                        setKubernetesMasterPort(443);
+                    }
+                })
+                .createPksCluster(),
+            new PksClusterBuilder()
+                .setUuid("539c7ba6-9e25-494f-99ad-4094a33ab633")
+                .setName("demo2")
+                .setLastAction("SUCCEEDED")
+                .setKubernetesMasterIps(Arrays.asList("13.113.212.19"))
+                .setParameters(new PksCluster.Parameters() {
+
+                    {
+                        setKubernetesMasterHost("demo2.pks.example.com");
+                        setKubernetesMasterPort(443);
+                    }
+                })
+                .createPksCluster());
     }
 
 }
